c5edaf2ee72072abc7e534abc026911921d38e1e
Rishab <singhrishab247@gmail.com>
2025-09-05 16:49:33 +0000

    conversion operations done

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -21,10 +21,53 @@
 // "" => false
 // "hi" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
+// let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ============== Operations ==============
+
+// let value = 3
+// let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "hello "
+// let str2 = "rishab"
+
+// let str3 = str1 + str2
+
+// console.log(str3);
+
+// primitive datatype - performon the basis of first number datatype
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 +2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+// prefix & postfix operator - js/mdn - done 
+// let gameCounter = 100
+// ++gameCounter;
+// gameCounter++
+
+// console.log(gameCounter);
+
+
+
+
+
+
 
