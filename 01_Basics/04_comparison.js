1995f9a4e10b10dd9a21447c439ca8e218c6be27
Rishab <singhrishab247@gmail.com>
2025-09-05 17:21:46 +0000

    datatype summary

--- 01_Basics/04_comparison.js
+++ 01_Basics/04_comparison.js
@@ -15,6 +15,6 @@
 
 // ===
 
-console.log("2" == 2);
-console.log("2" === 2);
+// console.log("2" == 2);
+// console.log("2" === 2);
 
---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+// Primitive Datatypes
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+// let Id = Symbol("123")
+// let anotherId = Symbol("123")
+
+// console.log(Id);
+// console.log(Id == anotherId);
+
+
+
+// Javascript => Static or Dynamic Typed ?? => Dynamic
+
+//Reference (Non Primitive) Datatypes
+
+// Arrays, Objects, Functions
+
+const Heroes = ["naagraj", "spidey","HARRYP"]
+
+let myObj = {
+    name: "Rishab",
+    age: "22"
+}
+
+const myFunction = function() {
+    console.log("Hello World");
+    
+}
+
+myFunction
+
+// typeof of non primitive are objects like object functions, object array etc
