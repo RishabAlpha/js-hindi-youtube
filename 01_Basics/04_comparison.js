ec9e69346f27f530341accc98ae1849539f0f4cb
Rishab <singhrishab247@gmail.com>
2025-09-05 17:01:38 +0000

    04 comparisons

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// check == & comparisions >< >=<= work differntly
+// reason , comparisions convert null to a number, treating it as 0
+// that's why null>0 => false, null >= 0 => true
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+
+
+// ===
+
+console.log("2" == 2);
+console.log("2" === 2);
+
