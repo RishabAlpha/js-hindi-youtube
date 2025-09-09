f11fb5cd4441349e3f77f85978a0ce08fb118142
Rishab <singhrishab247@gmail.com>
2025-09-09 16:52:50 +0000

    06_nums_and_maths_01

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,33 @@
+// const score = 400
+// console.log(score);
+
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 23.8965
+
+// console.log((otherNumber.toPrecision(2)))
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// ----------------MATHS LIBRARY---------------------
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.96));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,8,1,6,9));
+// console.log(Math.max(4,8,1,6,9));
+
+console.log(Math.random());
+
+
+
+
+
