e48e8aaa097e978bf34e8c7426760802c7068c59
Rishab <singhrishab247@gmail.com>
2025-09-09 14:48:35 +0000

    05_strings.js 01

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,41 @@
+const name = "Rishab"
+const repoCount = "1"
+
+// console.log(name + repoCount + "Value");
+
+// console.log(`Hello my Name is ${name} & my RepoCount is ${repoCount}`);
+
+const HypenName = new String("Hyper-nation-game-name")
+
+// console.log(HypenName[1]);
+// console.log(HypenName.__proto__);
+
+// console.log(HypenName.length);
+// console.log(HypenName.toUpperCase());
+// console.log(HypenName.charAt(2));
+// console.log(HypenName.indexOf('n'));
+
+// const newString = HypenName.substring(0,4)
+// console.log(newString);
+
+// const anotherString = HypenName.slice(-4, 2)
+// console.log(anotherString);
+
+// const yetNewString = "   Rishab   "
+// console.log(yetNewString.trim());
+// console.log(yetNewString);
+
+// const url = "https://rishab.com/info%20personal"
+
+// console.log(url.replace('%20','-'))
+// console.log(url);
+
+// console.log(url.includes('ham'));
+
+console.log(HypenName.split('-'));
+
+
+
+
+
+
