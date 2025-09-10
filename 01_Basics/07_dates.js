3c407e3b613c463fa742d1e2783e2f4d1ee7fc3b
Rishab <singhrishab247@gmail.com>
2025-09-10 14:15:38 +0000

    07_date.js

---
+++ 01_Basics/07_dates.js
@@ -0,0 +1,30 @@
+// DATES
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2025, 2, 23)
+// let myCreatedDate = new Date(2025, 2, 23, 5, 3)
+// let myCreatedDate = new Date("01-14-2025")
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+// let newDate = new Date()
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+
+// let newtype = newDate.toLocaleString('default', {
+//      weekday: "short",
+//      })
+
+// console.log(newtype);
+
