e49d3c6e5f4bc5fcc23fadebcec7fc6af77e6ec8
Rishab <singhrishab247@gmail.com>
2025-09-10 13:51:40 +0000

    06_nums_and_maths_02

--- 01_Basics/06_nums_and_maths.js
+++ 01_Basics/06_nums_and_maths.js
@@ -25,7 +25,13 @@
 // console.log(Math.min(4,8,1,6,9));
 // console.log(Math.max(4,8,1,6,9));
 
-console.log(Math.random());
+// console.log(Math.random());
+// console.log(Math.floor((Math.random()*10) + 1));
+
+// const min = 10
+// const max = 20
+
+// console.log(Math.floor(Math.random() * (max-min+1)+min))
 
 
 
