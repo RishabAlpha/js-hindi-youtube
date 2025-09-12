fb650c23fa8162d6124c3e0f42b782d023a717aa
Rishab <singhrishab247@gmail.com>
2025-09-12 15:38:04 +0000

    arrays 02

---
+++ 02_Basics/02_arrays.js
@@ -0,0 +1,26 @@
+const marvel_heroes = ["thor", "IronMan", "Spiderman"]
+const dc_heroes = ["superman","flash","batman"]
+
+// marvel_heroes.push(dc_heroes)
+
+// console.log(marvel_heroes);
+// console.log(marvel_heroes[3][1]);
+
+// const allHeroes = marvel_heroes.concat(dc_heroes)
+// console.log(allHeroes);
+
+// const all_new_Heroes = [...marvel_heroes,...dc_heroes]
+// console.log(all_new_Heroes);
+
+// const another_array = [1, 2, 3, [4,5,6], 7, [6,7[4,5]]]
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+// console.log(Array.from("India"));
+console.log(Array.from({name: "India"}));
+// console.log(Array.isArray("India"));
+
+
+
+
+ 
